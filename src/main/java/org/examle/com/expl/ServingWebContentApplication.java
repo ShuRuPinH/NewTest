f73ec2295e1afa01356f065c5d903773c0ea0f68
shurupin <shurupin@gmail.com>
2020-07-10 01:56:28 +0300

    1 st run

---
+++ src/main/java/org/examle/com/expl/ServingWebContentApplication.java
@@ -0,0 +1,15 @@
+package org.examle.com.expl;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
