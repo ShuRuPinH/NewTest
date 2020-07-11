5d4fd937abb087dfc2f383a1c5fdcdffd11b4c08
shurupin <shurupin@gmail.com>
2020-07-11 11:16:38 +0300

    + 2,3 strs + /12

--- src/main/java/org/examle/com/expl/GreetingController.java
+++ src/main/java/org/examle/com/expl/GreetingController.java
@@ -13,6 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
         @GetMapping("/")
         public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
             model.addAttribute("name", name);
+                       return "greeting";
+        }
+
+        @GetMapping("/12")
+        public String greeting2(Model a) {
+            a.addAttribute("txt", "WEEQWEQWE");
+            a.addAttribute("txt2", "tytryrtyrt");
+            a.addAttribute("txt3", "5434534534");
             return "greeting";
         }
 
