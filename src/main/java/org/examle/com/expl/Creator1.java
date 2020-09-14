70efd211d3d33a349afcf4708e11452cb9d190ff
shurupin <shurupin@gmail.com>
2020-09-14 19:28:26 +0300

    last edition - input without test

---
+++ src/main/java/org/examle/com/expl/Creator1.java
@@ -0,0 +1,26 @@
+package org.examle.com.expl;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Creator1 {
+
+  static String creat (){
+
+
+    Path p = Paths.get("test.html");
+    Path niw;
+
+    {
+        try {
+            niw = Files.createFile(p);
+        } catch (IOException e) {
+           return "не удалось";
+        }
+    }
+    return niw.toAbsolutePath().toString();
+}
+
+}
--- src/main/java/org/examle/com/expl/GreetingController.java
+++ src/main/java/org/examle/com/expl/GreetingController.java
@@ -5,6 +5,7 @@ package org.examle.com.expl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,8 +15,8 @@ import java.util.List;
     public class GreetingController {
 
         @GetMapping("/")
-        public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-            model.addAttribute("name", name);
+        public String greeting(@RequestParam String id, Model a) {
+            a.addAttribute("text", Creator1.creat());
                        return "test";
         }
 
